b0875da1265110f7cd069e17c154a88777fc4814
lkotlarenko <lkotlarenko@protonmail.com>
2021-12-03 12:54:30 -0300

    refactoring/removing unnecessary code

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -2,16 +2,14 @@
 const urlItem = (ItemID) => `https://api.mercadolibre.com/items/${ItemID}`;
 
 const fetchItem = async (item) => {
-  // if fetchItem is called with no parameter return an error
-  if (!item) throw new Error('You must provide an url');
   try {
     // fetch on api then return the result
     const data = await fetch(urlItem(item));
     const dataJson = await data.json();
     return dataJson;
   } catch (error) {
-    throw new Error(error);
-  }
+    throw new Error(error.message);
+   }
 };
 
 if (typeof module !== 'undefined') {
--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,17 +1,14 @@
 // function to get the correct api endpoint url
-const url = (search) =>
-  `https://api.mercadolibre.com/sites/MLB/search?q=${search}`;
+const url = (search) => `https://api.mercadolibre.com/sites/MLB/search?q=${search}`;
 
 const fetchProducts = async (theme) => {
-  // if fetchProducts is called with no parameter return an error
-  if (!theme) throw new Error('You must provide an url');
   try {
     // fetch on api then return the resulting array
     const data = await fetch(url(theme));
     const dataJson = await data.json();
     return dataJson.results;
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error.message);
   }
 };
 
